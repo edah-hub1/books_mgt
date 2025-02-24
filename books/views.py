926a4436c9c4fccc2a882b0557b19f07a90de154
edah-hub1 <cheruiyotedah024@gmail.com>
2025-02-24 15:43:42 +0300

    update README.md file

--- books/views.py
+++ books/views.py
@@ -8,14 +8,12 @@ from .serializers import BookSerializer
 
 
 class BookListCreateAPIView(generics.ListCreateAPIView):
-    """Handles listing all books and creating a new book."""
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    """Handles retrieving, updating, and deleting a book by ID."""
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
